0744631da77b6694d6d819c2c28e76768b2d31bd
Oguzhan Gurbuz <grboguz@gmail.com>
2025-04-23 11:15:43 +0000

    rviz configuration is created

--- robot_patrol/launch/start_patrolling.launch.py
+++ robot_patrol/launch/start_patrolling.launch.py
@@ -1,12 +1,27 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+import os
+from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
+    rviz_config_path = os.path.join(
+        get_package_share_directory('robot_patrol'),
+        'rviz',
+        'display.rviz'
+    )
+
     return LaunchDescription([
         Node(
-            package='robot_patrol', 
+            package='robot_patrol',
             executable='patrol',
             name='patrol_node',
             output='screen'
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+            arguments=['-d', rviz_config_path]
         )
     ])
